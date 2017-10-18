833fc6e526002aa9e06a46aaa554475794db9b16
Victor Sena Molero <victorsenam@gmail.com>
2017-10-18 21:29:05 -0200

    Tirango googletest

--- implementacao/testes/aux/FindRowMin.h
+++
@@ -1,9 +0,0 @@
-#ifndef TCC_FindRowMin
-#define TCC_FindRowMin
-
-#include <vector>
-#include <functional>
-
-std::vector<int> FindRowMin (std::function< double(int,int) > A, int n, int m);
-
-#endif
--- implementacao/testes/aux/SolveIntRec.h
+++
@@ -1,9 +0,0 @@
-#ifndef TCC_FindRowMin
-#define TCC_FindRowMin
-
-#include <vector>
-#include <functional>
-
-std::vector<std::vector<double> > SolveIntRec (std::function< double(int,int) > C, int n);
-
-#endif
