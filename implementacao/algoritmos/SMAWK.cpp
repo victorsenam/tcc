7e22b2481a6c50c6d8d33179243dba32d07efcfa
Victor Sena Molero <victorsenam@gmail.com>
2017-10-22 17:32:57 -0200

    Arrumando SMAWK

--- implementacao/algoritmos/SMAWK.cpp
+++ implementacao/algoritmos/SMAWK.cpp
@@ -1,12 +1,18 @@
 #include "SMAWK.h"
 #include <iostream>
+#include <cassert>
 
 SMAWK::SMAWK (std::function< double(int, int) > A, int n, int m) {
-	this->A = A;
 	this->n = n;
+	this->A = [A,m] (int i, int j) {
+		if (j >= m) return 1./0.;
+		else return A(i,j);
+	};
 	p = 1;
 	for (int i = 0; i < m; i++)
 		col.push_back(i);
+
+	Reduce();
 }
 
 void SMAWK::RemoveOddRows () {
@@ -17,22 +23,22 @@ void SMAWK::Reduce () {
 	int k = 0;
 	std::list<int>::iterator it = col.begin();
 
-
     while (col.size() > unsigned(n + p - 1)/p) {
-        if (A(p*k, *it) > A(p*k, *std::next(it))) {
+		assert(it != col.end() && std::next(it) != col.end());
+        if (A(p*k, *it) <= A(p*k, *std::next(it))) {
+			if (p*(k+1) >= n) {
+                col.erase(std::next(it));
+			} else {
+				it = std::next(it);
+				k++;
+			}
+		} else { 
 			it = std::next(it);
             it = col.erase(std::prev(it));
 			if (k != 0) {
 				k--;
 				it = std::prev(it);
 			}
-        } else {
-            if (p*(k+1) >= n) {
-                col.erase(std::next(it));
-            } else {
-                it = std::next(it);
-                k++;
-            }
         }
     }
 }
@@ -45,6 +51,7 @@ std::vector<int> SMAWK::FindRowMinima () {
 
 void SMAWK::FindRowMinima (std::vector<int> & R) {
 	if (p >= n) {
+		assert(col.size() == 1u);
 		R[0] = *(col.begin());
 	} else {
 		SMAWK B = (*this);
@@ -65,5 +72,6 @@ void SMAWK::FindRowMinima (std::vector<int> & R) {
 				it = std::next(it);
 			}
 		}
+
 	}
 }
